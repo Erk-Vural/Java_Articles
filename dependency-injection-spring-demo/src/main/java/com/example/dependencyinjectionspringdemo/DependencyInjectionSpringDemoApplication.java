7479422201cef7b5c4fcdda20c6fe8daaf293d64
Erk-Vural <er.bugrahan@gmail.com>
2022-03-12 00:28:50 +0300

    init

---
+++ dependency-injection-spring-demo/src/main/java/com/example/dependencyinjectionspringdemo/DependencyInjectionSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dependencyinjectionspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyInjectionSpringDemoApplication.class, args);
+    }
+
+}
---
+++ dependency-injection-spring-demo/src/test/java/com/example/dependencyinjectionspringdemo/DependencyInjectionSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependencyinjectionspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionSpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
